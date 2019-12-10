b282f3df2ab96d9f7d82984db08fa5b5bdce7db1
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2019-12-09 21:39:48 -0300

    Adding Singleton
    Adicionando Singleton

---
+++ Singleton/src/com/design/singleton/EnumSingleton.java
@@ -0,0 +1,12 @@
+package com.design.singleton;
+
+import java.io.Serializable;
+
+public enum EnumSingleton {
+
+    INSTANCE;
+    
+    public static void doSomething(){
+        //do something
+    }
+}
