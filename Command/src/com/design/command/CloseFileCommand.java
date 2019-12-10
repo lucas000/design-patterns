2eb86d27071c856efaeb3735904288fdb4826fb0
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2019-12-09 21:38:33 -0300

    Adding Command
    Adicionando padrão Command

---
+++ Command/src/com/design/command/CloseFileCommand.java
@@ -0,0 +1,15 @@
+package com.design.command;
+
+public class CloseFileCommand implements Command {
+
+	private FileSystemReceiver fileSystem;
+	
+	public CloseFileCommand(FileSystemReceiver fs){
+		this.fileSystem=fs;
+	}
+	@Override
+	public void execute() {
+		this.fileSystem.closeFile();
+	}
+
+}
